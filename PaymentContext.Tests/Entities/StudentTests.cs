5be7fc09ae8d8bec6d29c7aae0d57cb65d6b1f39
Moisés Alves <moisesalves1@gmail.com>
2023-09-07 15:40:22 -0300

    Evitando corrupção do código

---
+++ PaymentContext.Tests/Entities/StudentTests.cs
@@ -0,0 +1,15 @@
+using PaymentContext.Domain.Entities;
+
+namespace PaymentContext.Tests;
+
+[TestClass]
+public class StudentTests
+{
+    [TestMethod]
+    public void AdicionarAssinatura()
+    {
+        var subscription = new Subscription(null);
+        var student = new Student("Moises", "Silva","12345678912","hello@gmail.com");
+        student.AddSubscription(subscription);
+    }
+}
